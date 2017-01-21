a22c1e0a063e852bd020ccc8d4e1c78399cb639a
Phil-Sparkes <phil.sparkes95@gmail.com>
2017-01-21 18:35:35 +0000

    Add files via upload

---
+++ Assets/Code/IsDead.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class IsDead : StateMachineBehaviour {
+    int Playerdie = Animator.StringToHash("PlayerDie");
+    Animator anim;
+    void Start()
+    {
+       anim = GetComponent<Animator>();
+    }
+
+    private T GetComponent<T>()
+    {
+        throw new NotImplementedException();
+    }
+
+    // Update is called once per frame
+    bool Update()
+    {
+        GameObject Text = GameObject.Find("Text");
+        HealthScript targetScript = Text.GetComponent<HealthScript>();
+        if (targetScript.Health <= 0)
+        {
+            anim.SetTrigger(Playerdie);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
